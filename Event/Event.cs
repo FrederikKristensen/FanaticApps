2a69c654d4d7baf1b74e89fdb309e3745953936f
Frederik Kristensen <frede4947@gmail.com>
2020-05-12 10:59:05 +0200

    Opsætning af Event, Program og Worker

---
+++ Event/Event.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+    class Event
+    {
+        public int EventId { get; set; }
+
+        public string EventNavn { get; set; }
+
+        public int EventTid { get; set; }
+
+        public int EventDato { get; set; }
+
+        public string EventAdresse { get; set; }
+
+        public int EventPostnummer { get; set; }
+
+        public Event()
+        {
+        }
+
+        public Event(int eventId, string eventNavn, int eventTid, int eventDato, string eventAdresse, int eventPostnummer)
+        {
+            EventId = eventId;
+            EventNavn = eventNavn;
+            EventTid = eventTid;
+            EventDato = eventDato;
+            EventAdresse = eventAdresse;
+            EventPostnummer = eventPostnummer;
+        }
+
+        
+    } 
+}
---
+++ Event/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+    internal class Worker
+    {
+        static void Main(string[] args)
+        {
+            //private const string URL = "";
+
+            public void Start()
+            {
+                IList<Event> EventListe = await GetallEventAsync();
+            }
+        }
+    }
+}
