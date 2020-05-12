d6e0d71678172c53197b7d598c5ae837a0aa0f98
Frederik Kristensen <frede4947@gmail.com>
2020-05-12 14:29:26 +0200

    Lavet worker og klaregjort connection

--- Event/Worker.cs
+++ Event/Worker.cs
@@ -1,19 +1,36 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Event
 {
     internal class Worker
     {
-        static void Main(string[] args)
-        {
-            //private const string URL = "";
+        private const string URI = "http://localhost:51306/api/Events";
+        //private const string URI = "";
 
-            public void Start()
+        public async void Start()
+        {
+            IList<Event> EventListe = await GetAllEventAsync();
+            foreach (Event eve in EventListe)
             {
-                IList<Event> EventListe = await GetallEventAsync();
+                Console.WriteLine(eve);
             }
         }
+
+        private async Task<IList<Event>> GetAllEventAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                String content = await client.GetStringAsync(URI);
+                IList<Event> clist = JsonConvert.DeserializeObject<IList<Event>>(content);
+                return clist;
+            }
+
+        }
+
     }
 }
